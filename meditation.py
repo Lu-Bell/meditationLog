2992541e6ab901747aaaaa29d0ca089f4734147a
Luther <luther.bell17@stjohns.edu>
2021-01-27 14:36:25 -0500

    Updated -- 1/27/2020

--- meditation.py
+++ meditation.py
@@ -1,1 +1,5 @@
-print("Hello World")
+print("This is Meditation Application to Log Your Meditation practices"
+user_date = input("First enter a date")
+user_time = input("Now enter how long you meditated for")
+user_info = "Today ("+user_date+") you meditated for "+user_time+" minutes!"
+print(user_info)
